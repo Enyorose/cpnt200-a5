dfd90677607789e1a22c1a8a87c5a6775dfd8e2b
Jessica.W <jessica.watson1@live.com>
2021-11-29 14:38:15 -0700

    fixed card information

--- pages/members.vue
+++ pages/members.vue
@@ -1,35 +1,41 @@
 <template>
   <v-main>
     <v-container>
-    
-    <Header :pageInfo="pageInfo"/>
-    <Card :card="cardInfo"/>
-    <Footer />
+      <Header :pageInfo="pageInfo" />
+      <Card :card="card" v-for="card in cards" :key="card.id" />
+      <Footer />
     </v-container>
   </v-main>
 </template>
 <style scoped>
-  /*
+/*
    *  Custom styles for the page
    */
 </style>
 <script>
-
 export default {
   data() {
     return {
       // Custom page data comes here.
       pageInfo: {
-        menuName: 'members',
+        menuName: "members",
       },
-      cardInfo: {
-        name: 'Jessica Watson',
-        title: 'Backend',
-        description: 'Set up and managed server information as well as designed members page.',
-        filter: ['#family', '#photographer', '#developer']
-      }
-    }
+      cards: [
+        {
+          name: "Jessica Watson",
+          title: "Backend",
+          description:
+            "Set up and managed server information as well as designed members page.",
+        },
+        {
+          name: "Jessica Watson",
+          title: "Backend",
+          description:
+            "Set up and managed server information as well as designed members page.",
+          filter: ["#family", "#photographer", "#developer"],
+        },
+      ],
+    };
   },
-  
-}
+};
 </script>
