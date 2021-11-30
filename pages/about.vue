c1ae87d09d9f734ed3e85cf449e5576f65757893
Jessica.W <jessica.watson1@live.com>
2021-11-30 14:01:44 -0700

    working on about me page

--- pages/about.vue
+++ pages/about.vue
@@ -1,32 +1,25 @@
 <template>
   <v-main>
-   
-    <Header :pageInfo="pageInfo"/>
-    About page....
+    <Header :pageInfo="pageInfo" />
+    <Subheader :page="pageInfo" />
     <Footer />
-    
   </v-main>
 </template>
 <style scoped>
-  /*
+/*
    *  Custom styles for the page
    */
 </style>
 <script>
 export default {
-
   data() {
     return {
       // Custom page data comes here.
       pageInfo: {
-        menuName: 'about',
-      }
-    }
+        menuName: "about",
+        header: "About Us",
+      },
+    };
   },
-  async asyncData ({ $content }) {
-
-    return {
-    }
-  }
-}
+};
 </script>
