ed38cb229d1546de2ae34d55c970d2918629d63d
Jessica.W <jessica.watson1@live.com>
2021-11-30 19:26:56 -0700

    added css to about page

--- components/InputComment.vue
+++ components/InputComment.vue
@@ -1,10 +1,17 @@
 <template>
-  <label :for="inputInfo.inputName">{{inputInfo.label}}
-    <input class=" border-0" type="text" :name="inputInfo.inputName" :value="value" @input="$emit('input', $event.target.value)">
+  <label :for="inputInfo.inputName" class="flex flex-col"
+    >{{ inputInfo.label }}
+    <input
+      class="border-2 mt-3"
+      type="text"
+      :name="inputInfo.inputName"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
   </label>
 </template>
 <script>
 export default {
-  props: ['inputInfo', 'value']
-}
+  props: ["inputInfo", "value"],
+};
 </script>
--- pages/about.vue
+++ pages/about.vue
@@ -5,24 +5,29 @@
       <Subheader :page="pageInfo" />
       <article class="max-w-3xl mx-auto mt-4">
         <p class="text-base">
-          Our team of developers are dedicated to bringing you the highest quality
-          content on the most up to date technology. We do all the research and
-          summarize it, so you can make the most informed choices for your own
-          project.
+          Our team of developers are dedicated to bringing you the highest
+          quality content on the most up to date technology. We do all the
+          research and summarize it, so you can make the most informed choices
+          for your own project.
         </p>
       </article>
       <article class="max-w-3xl mx-auto mt-5">
         <p class="text-base">
-          Here is an opportunity to tell us what you would like to see next. Fill
-          out this short form and keep an eye out we may cover your topic next!
+          Here is an opportunity to tell us what you would like to see next.
+          Fill out this short form and keep an eye out we may cover your topic
+          next!
         </p>
-        <form @submit.prevent="addComment">
+        <form
+          @submit.prevent="addComment"
+          class="mt-5 flex flex-col items-center border-2 max-w-md"
+        >
           <InputComment
             :value="message.msg"
             :inputInfo="message"
             v-model="message.msg"
+            class="border-0"
           />
-          <button class="bg-gray-400">Add Comment</button>
+          <button class="bg-gray-400 mt-5">Add Comment</button>
         </form>
         <ul>
           <li
@@ -54,7 +59,7 @@ export default {
       },
       message: {
         msg: "",
-        label: "Comment",
+        label: "We are interested in your idea!",
         inputName: "input comment",
         comment: "",
       },
